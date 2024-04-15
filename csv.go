654ba168668d69eecc3e3428b461248b3ad93473
Ashot Karapetian <AshotArtashesi@yandex.ru>
2024-04-15 11:51:55 +0300

    csv2float() mem improvement

--- csv.go
+++ csv.go
@@ -27,18 +27,21 @@ func avg(data []float64) float64 {
 func csv2float(r io.Reader, column int) ([]float64, error) {
 	// Create the CSV Reader used to read in data from CSV files
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 
 	// Adjusting for 0 based index
 	column--
 
-	// Read all CSV data
-	allData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("cannot read data from file: %w", err)
-	}
-
 	var data []float64
-	for i, row := range allData {
+	for i := 0; ; i++ {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
+
 		if i == 0 {
 			continue
 		}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module colStats
 
 go 1.19
+
+require (
+	golang.org/x/tools v0.20.0 // indirect
+)
